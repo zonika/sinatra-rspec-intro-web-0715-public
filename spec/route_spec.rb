f4844f152fba181266b1c2609613434731cb87fc
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-21 18:03:55 -0400

    further comment updates in route_spec

--- spec/route_spec.rb
+++ spec/route_spec.rb
@@ -2,8 +2,10 @@ require_relative 'spec_helper'
 
 describe "routes" do
 
-  #this is something that Rack::Test needs defined
-  #in order to run tests against the Sinatra App
+  # this is something that Rack::Test needs defined
+  # in order to run tests against the Sinatra App
+  # you'll see it inside each of our top-level describes
+
   def app
     TestApp
   end
@@ -20,17 +22,23 @@ describe "routes" do
     # 'get' is a helper from Rack::Test
     # which is included in spec_helper.
     # it specifies a get request.
+
     get '/'
+
     # be_ok is a helper from Rack::Test
+
     expect(last_response).to be_ok
+
     # be_ok is the equivalent
     # of checking for HTTP status code 200
+
     expect(last_response.status).to eq(200)
   end
 
   it "handles POST to /" do
     # 'post' is a helper from Rack::Test
     # it specifies a post request.
+
     post '/'
     expect(last_response).to be_ok
   end
