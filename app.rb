d59f5fa4d6220432a1b4c8b8a4bb827eeb03aab2
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-22 11:05:56 -0400

    added solution and hello template

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require 'sinatra'
 class TestApp < Sinatra::Base
   #put your route handlers here
 
-  get '/' do
+  get '/' do 
   end
 
+  post '/' do
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  get '/hello' do
+    @name = params[:name]
+    erb :hello
+  end
 end
