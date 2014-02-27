ccd8c5d5f463ee66d18ecfb02e3dd53def176dd8
Avi Flombaum <avi@flombaum.com>
2014-02-27 14:46:43 -0500

    Removed solution

--- app.rb
+++ app.rb
@@ -2,19 +2,4 @@ require 'sinatra'
 
 class TestApp < Sinatra::Base
   #put your route handlers here
-
-  get '/' do 
-  end
-
-  post '/' do
-  end
-
-  get '/about' do
-    erb :about
-  end
-
-  get '/hello' do
-    @name = params[:name]
-    erb :hello
-  end
 end
